1fd860a12eb09bfc01c1caa5da21070392ce4d0b
Garr777 <redka17@mail.ru>
2021-11-25 18:15:04 +0300

    Add solutions for tusks 1-5

--- main.cpp
+++ main.cpp
@@ -1,3 +1,45 @@
-int main()
+#include<iostream>
+#include<tuple>
+#include<vector>
+void log(const char* &str)
 {
+  for (int i = 0, i < str.length();i++)
+  {
+    std::cout << i;
+  }
+}
+
+void swap(auto a, auto b)
+{
+  auto t = &a;
+  &a = &b;
+  &b = t;
+}
+
+constexpr fact(int n)
+{
+  if (n > 0)
+    return n * fact(n - 1);
+  return 1;
+}
+std::vector<int> naturals(int n)
+{
+  std::vector<int> vec;
+  for (int i = 1; i <= n; i++)
+  {
+    vec.insert(n);
+  }
+  return vec;
+}
+
+std::tuple<char*, char*> returnFL() 
+{ 
+  std::tuple a = [ "—абиров", "–ишат" ];
+  return a;
+}
+
+int main()
+  {
+  //fact(33) - выполн€етс€ во врем€ компил€ции;
+  //int a; std::cin >> a; fact(a) - выполн€етс€ во врем€ рантайма;
 }
