41f568f490f71b9450344381af957a2ad6fe380a
Garr777 <redka17@mail.ru>
2021-12-16 08:37:18 +0300

    HW by Sabirov Rishat

--- main.cpp
+++ main.cpp
@@ -1,45 +1,107 @@
-#include<iostream>
-#include<tuple>
-#include<vector>
-void log(const char* &str)
+#include <iostream>
+#include <tuple>
+#include <vector>
+#include <string>
+#include <chrono>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+
+void Task1(const std::string& str)
 {
-  for (int i = 0, i < str.length();i++)
-  {
-    std::cout << i;
-  }
+  std::cout << str;
 }
 
-void swap(auto a, auto b)
+void Task2()
 {
-  auto t = &a;
-  &a = &b;
-  &b = t;
+  auto swap = [](auto& a, auto& b) {
+    auto t = a;
+    a = b;
+    b = t;
+  };
+
+  int a = 5;
+  int b = 6;
+  swap(a, b);
+
+  std::cout << a << std::endl;
+  std::cout << b << std::endl;
+
+  std::string s1("W");
+  std::string s2("B");
+  swap(s1, s2);
+
+  std::cout << s1 << std::endl;
+  std::cout << s2 << std::endl;
 }
 
-constexpr fact(int n)
+auto Credentials(const std::string& name, const std::string& lastName)
 {
-  if (n > 0)
-    return n * fact(n - 1);
-  return 1;
+  return std::pair<const std::string&, const std::string&>{ name, lastName };
 }
-std::vector<int> naturals(int n)
+
+void Task3()
 {
-  std::vector<int> vec;
-  for (int i = 1; i <= n; i++)
-  {
-    vec.insert(n);
-  }
+  auto test{ Credentials("Rishat", "Sabirov") };
+
+  std::string name{ std::get<0>(test) };
+  std::string lastName{ std::get<1>(test) };
+}
+
+constexpr int fact(int n)
+{
+  return n == 1 ? 1 : n * fact(n - 1);
+}
+
+void Task4()
+{
+  auto start_static{ std::chrono::high_resolution_clock::now() };
+
+  auto result_static = fact(33);
+  std::cout << result_static << std::endl;
+
+  std::chrono::duration<double> time_static{ std::chrono::high_resolution_clock::now() - start_static };
+  std::cout << "static factorial time: " << std::fixed << std::setprecision(6) << time_static.count() << std::endl;
+
+  std::cout << "Enter a number for finding factorial" << std::endl;
+  int n;
+  std::cin >> n;
+
+  auto start{ std::chrono::high_resolution_clock::now() };
+
+  auto result = fact(n);
+  std::cout << result << std::endl;
+
+  std::chrono::duration<double> time{ std::chrono::high_resolution_clock::now() - start };
+  std::cout << "runtime factorial time: " << std::fixed << std::setprecision(6) << time.count() << std::endl;
+}
+
+auto Naturals(int n)
+{
+  std::vector<int> vec(n);
+  std::generate(vec.begin(), vec.end(), []() {
+    static int i = 1;
+    return i++;
+  });
   return vec;
 }
-
-std::tuple<char*, char*> returnFL() 
-{ 
-  std::tuple a = [ "—абиров", "–ишат" ];
-  return a;
+void Task5()
+{
+  std::cout << "Enter a number for create vector" << std::endl;
+  int n;
+  std::cin >> n;
+  auto vect = Naturals(n);
+  for (auto y : vect)
+  {
+    std::cout << y << std::endl;
+  }
 }
 
 int main()
-  {
-  //fact(33) - выполн€етс€ во врем€ компил€ции;
-  //int a; std::cin >> a; fact(a) - выполн€етс€ во врем€ рантайма;
+{
+  Task1("Digital device");
+  Task2();
+  Task3();
+  Task4();
+  Task5();
 }
